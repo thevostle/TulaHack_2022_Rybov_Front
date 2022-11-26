521986500dcce7a8e9adc08ea8f5f70b95976daa
thevostle <technic.dima@gmail.com>
2022-11-26 15:29:45 +0300

    разделил бэк от фронта

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+	mode: 'history',
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'Home',
+			component: Home,
+		},
+		{
+			path: '/auth/',
+			name: 'Auth',
+			component: () => import('../views/Auth.vue'),
+		},
+		{
+			path: '/profile/:userID(\\d+)',
+			name: 'User profile',
+			component: () => import('../views/UserProfile.vue'),
+		},
+		{
+			path: '/room/:roomId(\\d+)',
+			name: 'Room',
+			component: () => import('../views/Room.vue'),
+		},
+		{
+			path: '/createRoom',
+			name: 'Room creation',
+			component: () => import('../views/NewRoom.vue'),
+		},
+	],
+});
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<script>
+import HomeAside from '../components/layout/HomeAside.vue';
+import Browser from '../components/layout/Browser.vue';
+
+export default {
+	components: {
+		HomeAside,
+		Browser,
+	},
+};
+</script>
+
+<template>
+	<div>
+		<HomeAside />
+		<Browser />
+	</div>
+</template>
